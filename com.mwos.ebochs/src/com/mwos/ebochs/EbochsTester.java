7fb4bdcfafb8f9d554f794a8a71c502d27357065
aaronmaomao <1076249537@qq.com>
2018-03-04 21:37:37 +0800

    UPDATE

---
+++ com.mwos.ebochs/src/com/mwos/ebochs/EbochsTester.java
@@ -0,0 +1,13 @@
+package com.mwos.ebochs;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+public class EbochsTester extends PropertyTester {
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+
+		return false;
+	}
+
+}
