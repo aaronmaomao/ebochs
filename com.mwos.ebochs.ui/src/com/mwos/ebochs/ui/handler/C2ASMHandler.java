6e23b0cf3b76603cfe1d5b323b6b23f691be51a5
zhengjun.mao.ry <zhengjun.mao.ry@renesas.com>
2018-04-22 16:53:43 +0800

    update

---
+++ com.mwos.ebochs.ui/src/com/mwos/ebochs/ui/handler/C2ASMHandler.java
@@ -0,0 +1,15 @@
+package com.mwos.ebochs.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class C2ASMHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ com.mwos.ebochs.ui/src/com/mwos/ebochs/ui/handler/C2GASHandler.java
@@ -0,0 +1,16 @@
+package com.mwos.ebochs.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.w3c.dom.views.AbstractView;
+
+public class C2GASHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+}
