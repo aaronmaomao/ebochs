96c8fd37f0e6394499f6c3fee5243f30c7d3bb23
aaronmaomao <1076249537@qq.com>
2018-05-21 22:48:11 +0800

    update

--- com.mwos.ebochs/src/com/mwos/ebochs/resource/project/OSProjectNature.java
+++ com.mwos.ebochs/src/com/mwos/ebochs/resource/project/OSProjectNature.java
@@ -1,6 +1,11 @@
 package com.mwos.ebochs.resource.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
@@ -13,12 +18,25 @@ public class OSProjectNature implements IProjectNature {
 
 	@Override
 	public void configure() throws CoreException {
+		IProjectDescription des = project.getDescription();
+		ICommand newCommand = des.newCommand();
+		newCommand.setBuilderName(ProjectBuilder.ID);
+		des.setBuildSpec(new ICommand[] { newCommand });
+		project.setDescription(des, null);
 	}
 
 	@Override
 	public void deconfigure() throws CoreException {
-		// TODO Auto-generated method stub
-
+		IProjectDescription des = project.getDescription();
+		ICommand[] commands = des.getBuildSpec();
+		List<ICommand> cmds = new ArrayList<>();
+		for (ICommand command : commands) {
+			if (!command.getBuilderName().equals(ProjectBuilder.ID)) {
+				cmds.add(command);
+			}
+		}
+		des.setBuildSpec((ICommand[]) cmds.toArray());
+		project.setDescription(des, null);
 	}
 
 	@Override
@@ -30,6 +48,5 @@ public class OSProjectNature implements IProjectNature {
 	@Override
 	public void setProject(IProject project) {
 		this.project = project;
-
 	}
 }
