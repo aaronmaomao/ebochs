90d3ad9c95434cac6ed95ef59885c5d0777ee3f0
arronmaomao <1076249537@qq.com>
2018-07-22 23:48:07 +0800

    update

--- com.mwos.ebochs/src/com/mwos/ebochs/ui/view/A.java
+++ com.mwos.ebochs/src/com/mwos/ebochs/ui/view/A.java
@@ -1,32 +1,31 @@
 package com.mwos.ebochs.ui.view;
 
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.ExpandBar;
-import org.eclipse.swt.widgets.ExpandItem;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.wb.swt.ResourceManager;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.jface.layout.TableColumnLayout;
-import org.eclipse.jface.viewers.ColumnPixelData;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.TableEditor;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class A {
 
 	protected Shell shell;
 	private Table table;
-	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-	private Table table_1;
 
 	/**
 	 * Launch the application.
@@ -61,119 +60,86 @@ public class A {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(635, 484);
+		shell.setImage(ResourceManager.getPluginImage("com.mwos.ebochs", "resource/icons/arrow_refresh_small.png"));
+		shell.setSize(543, 561);
 		shell.setText("SWT Application");
-		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		shell.setLayout(new GridLayout(1, false));
 		
-		ExpandBar expandBar = new ExpandBar(shell, SWT.NONE);
-		expandBar.setToolTipText("");
-		
-		ExpandItem genRegExp = new ExpandItem(expandBar, SWT.NONE);
-		genRegExp.setExpanded(true);
-		genRegExp.setText("通用寄存器");
-		
-		Composite composite = new Composite(expandBar, SWT.NONE);
-		genRegExp.setControl(composite);
-		TableColumnLayout tcl_composite = new TableColumnLayout();
-		composite.setLayout(tcl_composite);
-		
-		TableViewer tableViewer = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION);
-		tableViewer.setColumnProperties(new String[] {});
+		TableViewer tableViewer = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
 		table = tableViewer.getTable();
-		table.setToolTipText("asd");
-		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		table.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
 		
-		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
-		tcl_composite.setColumnData(tblclmnNewColumn, new ColumnPixelData(60, true, true));
-		tblclmnNewColumn.setText("寄存器");
+		TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+		tableViewerColumn.setEditingSupport(new EditingSupport(tableViewer) {
+			protected boolean canEdit(Object element) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			protected CellEditor getCellEditor(Object element) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			protected Object getValue(Object element) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			protected void setValue(Object element, Object value) {
+				// TODO Auto-generated method stub
+			}
+		});
+		TableColumn tcReg1 = tableViewerColumn.getColumn();
+		tcReg1.setWidth(60);
+		tcReg1.setText("寄存器");
 		
-		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
-		tcl_composite.setColumnData(tblclmnNewColumn_1, new ColumnPixelData(120, true, true));
-		tblclmnNewColumn_1.setText("值");
+		TableViewerColumn tableViewerColumn_1 = new TableViewerColumn(tableViewer, SWT.NONE);
 		
-		TableColumn tblclmnNewColumn_2 = new TableColumn(table, SWT.NONE);
-		tcl_composite.setColumnData(tblclmnNewColumn_2, new ColumnPixelData(60, true, true));
-		tblclmnNewColumn_2.setText("寄存器");
+		TableColumn tcValue1 = tableViewerColumn_1.getColumn();
+		tcValue1.setWidth(100);
+		tcValue1.setText("值");
 		
-		TableColumn tblclmnNewColumn_3 = new TableColumn(table, SWT.NONE);
-		tcl_composite.setColumnData(tblclmnNewColumn_3, new ColumnPixelData(120, true, true));
-		tblclmnNewColumn_3.setText("值");
+		TableViewerColumn tableViewerColumn_2 = new TableViewerColumn(tableViewer, SWT.NONE);
+		TableColumn tcReg2 = tableViewerColumn_2.getColumn();
+		tcReg2.setWidth(60);
+		tcReg2.setText("寄存器");
 		
-		TableItem tableItem = new TableItem(table, SWT.NONE);
-		tableItem.setText(new String[] {"eax", "0x00000000", "ebp", "0x00000000"});
+		TableViewerColumn tableViewerColumn_3 = new TableViewerColumn(tableViewer, SWT.NONE);
+		TableColumn tcValue2 = tableViewerColumn_3.getColumn();
+		tcValue2.setWidth(100);
+		tcValue2.setText("值");
 		
-		TableItem tableItem_1 = new TableItem(table, 0);
-		tableItem_1.setText(new String[] {"ebx", "0x00000000", "esp", "0x00000000"});
+		TableItem tiEax = new TableItem(table, SWT.NONE);
+		tiEax.setText(new String[] {"eax", "0x00000000", "ebp", "0x00000000"});
 		
-		TableItem tableItem_2 = new TableItem(table, 0);
-		tableItem_2.setImage(2,ResourceManager.getPluginImage("com.mwos.ebochs", "resource/icons/basket.png"));
-		//tableItem_2.setText(new String[] {"ecx", "0x00000000", "esi", "0x00000000"});
+		TableItem tiEcx = new TableItem(table, 0);
+		tiEcx.setText(new String[] {"ecx", "0x00000000", "esp", "0x00000000"});
 		
-		TableEditor editor = new TableEditor(table);
-		Text te = new Text(table, SWT.NONE);
-		te.setEditable(false);
-		te.setText("vvv");
-		te.setSize(120, 0);
-	     editor.horizontalAlignment = SWT.LEFT;
-		editor.setEditor(te, tableItem_2, 3);
+		TableItem tiEdx = new TableItem(table, 0);
+		tiEdx.setText(new String[] {"edx", "0x00000000", "esi", "0x00000000"});
 		
-		TableItem tableItem_3 = new TableItem(table, 0);
-		tableItem_3.setText(new String[] {"edx", "0x00000000", "edi", "0x00000000"});
+		TableItem tiEbx = new TableItem(table, 0);
+		tiEbx.setText(new String[] {"ebx", "0x00000000", "edi", "0x00000000"});
 		
 		TableItem tableItem_4 = new TableItem(table, 0);
 		tableItem_4.setText(new String[] {});
 		
-		TableItem tableItem_5 = new TableItem(table, 0);
-		tableItem_5.setText(new String[] {"cs", "0xf000", "eip", "0x00000000"});
+		TableItem tiCs = new TableItem(table, 0);
+		tiCs.setText(new String[] {"cs", "0x0000", "eip", "0x00000000"});
 		
-		TableItem tableItem_6 = new TableItem(table, 0);
-		tableItem_6.setText(new String[] {"ss", "0x0000", "esp", "0x00000000"});
+		TableItem tiSs = new TableItem(table, 0);
+		tiSs.setText(new String[] {"ss", "0x0000", "esp", "0x00000000"});
 		
-		TableItem tableItem_7 = new TableItem(table, 0);
-		tableItem_7.setText(new String[] {"ds", "0x0000", "fs", "0x00000000"});
+		TableItem tiDs = new TableItem(table, 0);
+		tiDs.setText(new String[] {"ds", "0x0000", "es", "0x0000"});
 		
-		TableItem tableItem_8 = new TableItem(table, 0);
-		tableItem_8.setText(new String[] {"es", "0x0000", "gs", "0x00000000"});
-		genRegExp.setHeight(genRegExp.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		TableItem tiFs = new TableItem(table, 0);
+		tiFs.setText(new String[] {"fs", "0x0000", "gs", "0x0000"});
 		
-		ExpandItem segRegExp = new ExpandItem(expandBar, SWT.NONE);
-		segRegExp.setExpanded(true);
-		segRegExp.setText("控制寄存器");
-		
-		Composite composite_1 = new Composite(expandBar, SWT.NONE);
-		segRegExp.setControl(composite_1);
-		formToolkit.adapt(composite_1);
-		formToolkit.paintBordersFor(composite_1);
-		TableColumnLayout tcl_composite_1 = new TableColumnLayout();
-		composite_1.setLayout(tcl_composite_1);
-		
-		table_1 = new Table(composite_1, SWT.BORDER | SWT.FULL_SELECTION);
-		formToolkit.adapt(table_1);
-		formToolkit.paintBordersFor(table_1);
-		table_1.setHeaderVisible(true);
-		table_1.setLinesVisible(true);
-		
-		TableColumn tableColumn = new TableColumn(table_1, SWT.NONE);
-		tableColumn.setText("寄存器");
-		tcl_composite_1.setColumnData(tableColumn, new ColumnPixelData(60, true, true));
-		
-		TableColumn tableColumn_1 = new TableColumn(table_1, SWT.NONE);
-		tableColumn_1.setText("值");
-		tcl_composite_1.setColumnData(tableColumn_1, new ColumnPixelData(120, true, true));
-		
-		TableColumn tableColumn_2 = new TableColumn(table_1, SWT.NONE);
-		tableColumn_2.setText("寄存器");
-		tcl_composite_1.setColumnData(tableColumn_2, new ColumnPixelData(60, true, true));
-		
-		TableColumn tableColumn_3 = new TableColumn(table_1, SWT.NONE);
-		tableColumn_3.setText("值");
-		tcl_composite_1.setColumnData(tableColumn_3, new ColumnPixelData(120, true, true));
-		
-		TableItem tableItem_9 = new TableItem(table_1, 0);
-		tableItem_9.setText(new String[] {"eax", "0x00000000", "ebp", "0x00000000"});
-		segRegExp.setHeight(segRegExp.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		Button btnNewButton = new Button(shell, SWT.TOGGLE);
+		btnNewButton.setFont(SWTResourceManager.getFont("微软雅黑", 5, SWT.NORMAL));
+		btnNewButton.setAlignment(SWT.LEFT);
+		btnNewButton.setBackgroundImage(ResourceManager.getPluginImage("com.mwos.ebochs", "resource/icons/arrow_refresh_small.png"));
 
 	}
-
 }
--- com.mwos.ebochs/src/com/mwos/ebochs/ui/view/RegisterView.java
+++ com.mwos.ebochs/src/com/mwos/ebochs/ui/view/RegisterView.java
@@ -2,6 +2,10 @@ package com.mwos.ebochs.ui.view;
 
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -10,11 +14,26 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.jface.action.Action;
+import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.swt.layout.FillLayout;
+import swing2swt.layout.FlowLayout;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.MouseTrackAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class RegisterView extends ViewPart {
 
 	public static final String ID = "com.mwos.ebochs.ui.view.RegisterView"; //$NON-NLS-1$
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+	private Table table;
+	private Action actionRefresh;
 
 	public RegisterView() {
 	}
@@ -26,12 +45,106 @@ public class RegisterView extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new GridLayout(1, false));
+		container.setBackgroundMode(SWT.INHERIT_DEFAULT);
+		GridLayout gl_container = new GridLayout(1, false);
+		gl_container.marginWidth = 0;
+		gl_container.marginHeight = 0;
+		container.setLayout(gl_container);
 		{
 			Section sctnNewSection = formToolkit.createSection(container, Section.TWISTIE | Section.TITLE_BAR);
 			sctnNewSection.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 			formToolkit.paintBordersFor(sctnNewSection);
 			sctnNewSection.setText("通用寄存器");
+			Composite cs = formToolkit.createComposite(sctnNewSection);
+			sctnNewSection.setClient(cs);
+			cs.setLayout(new GridLayout(1, false));
+			{
+				
+				TableViewer tableViewer = new TableViewer(cs, SWT.BORDER | SWT.FULL_SELECTION);
+				table = tableViewer.getTable();
+				table.setHeaderVisible(true);
+				table.setLinesVisible(true);
+				table.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
+				
+				TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+				tableViewerColumn.setEditingSupport(new EditingSupport(tableViewer) {
+					protected boolean canEdit(Object element) {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					protected CellEditor getCellEditor(Object element) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					protected Object getValue(Object element) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					protected void setValue(Object element, Object value) {
+						// TODO Auto-generated method stub
+					}
+				});
+				TableColumn tcReg1 = tableViewerColumn.getColumn();
+				tcReg1.setWidth(60);
+				tcReg1.setText("寄存器");
+				
+				TableViewerColumn tableViewerColumn_1 = new TableViewerColumn(tableViewer, SWT.NONE);
+				
+				TableColumn tcValue1 = tableViewerColumn_1.getColumn();
+				tcValue1.setWidth(100);
+				tcValue1.setText("值");
+				
+				TableViewerColumn tableViewerColumn_2 = new TableViewerColumn(tableViewer, SWT.NONE);
+				TableColumn tcReg2 = tableViewerColumn_2.getColumn();
+				tcReg2.setWidth(60);
+				tcReg2.setText("寄存器");
+				
+				TableViewerColumn tableViewerColumn_3 = new TableViewerColumn(tableViewer, SWT.NONE);
+				TableColumn tcValue2 = tableViewerColumn_3.getColumn();
+				tcValue2.setWidth(100);
+				tcValue2.setText("值");
+				
+				TableItem tiEax = new TableItem(table, SWT.NONE);
+				tiEax.setText(new String[] {"eax", "0x00000000", "ebp", "0x00000000"});
+				
+				TableItem tiEcx = new TableItem(table, 0);
+				tiEcx.setText(new String[] {"ecx", "0x00000000", "esp", "0x00000000"});
+				
+				TableItem tiEdx = new TableItem(table, 0);
+				tiEdx.setText(new String[] {"edx", "0x00000000", "esi", "0x00000000"});
+				
+				TableItem tiEbx = new TableItem(table, 0);
+				tiEbx.setText(new String[] {"ebx", "0x00000000", "edi", "0x00000000"});
+				
+				TableItem tableItem_4 = new TableItem(table, 0);
+				tableItem_4.setText(new String[] {});
+				
+				TableItem tiCs = new TableItem(table, 0);
+				tiCs.setText(new String[] {"cs", "0x0000", "eip", "0x00000000"});
+				
+				TableItem tiSs = new TableItem(table, 0);
+				tiSs.setText(new String[] {"ss", "0x0000", "esp", "0x00000000"});
+				
+				TableItem tiDs = new TableItem(table, 0);
+				tiDs.setText(new String[] {"ds", "0x0000", "es", "0x0000"});
+				
+				TableItem tiFs = new TableItem(table, 0);
+				tiFs.setText(new String[] {"fs", "0x0000", "gs", "0x0000"});
+			}
+			
+			Composite composite = new Composite(sctnNewSection, SWT.NONE);
+			composite.setSize(10, 10);
+			sctnNewSection.setTextClient(composite);
+			composite.setLayout(new FillLayout(SWT.HORIZONTAL));
+			
+			Label lblNewLabel_1 = new Label(composite, SWT.HORIZONTAL | SWT.CENTER);
+			lblNewLabel_1.addMouseTrackListener(new MouseTrackAdapter() {
+				@Override
+				public void mouseEnter(MouseEvent e) {
+				}
+			});
+			lblNewLabel_1.setImage(ResourceManager.getPluginImage("com.mwos.ebochs", "resource/icons/add.png"));
+			
 		}
 		{
 			Section sctnNewSection_1 = formToolkit.createSection(container, Section.TWISTIE | Section.TITLE_BAR);
@@ -50,6 +163,11 @@ public class RegisterView extends ViewPart {
 	 */
 	private void createActions() {
 		// Create the actions
+		{
+			actionRefresh = new Action("refresh") {
+			};
+			actionRefresh.setImageDescriptor(ResourceManager.getPluginImageDescriptor("com.mwos.ebochs", "resource/icons/arrow_refresh_small.png"));
+		}
 	}
 
 	/**
@@ -57,6 +175,7 @@ public class RegisterView extends ViewPart {
 	 */
 	private void initializeToolBar() {
 		IToolBarManager toolbarManager = getViewSite().getActionBars().getToolBarManager();
+		toolbarManager.add(actionRefresh);
 	}
 
 	/**
@@ -64,11 +183,11 @@ public class RegisterView extends ViewPart {
 	 */
 	private void initializeMenu() {
 		IMenuManager menuManager = getViewSite().getActionBars().getMenuManager();
+		menuManager.add(actionRefresh);
 	}
 
 	@Override
 	public void setFocus() {
 		// Set the focus
 	}
-
 }
