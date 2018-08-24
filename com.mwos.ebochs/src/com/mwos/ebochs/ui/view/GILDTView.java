193876449c34e87b615e765fb92b4a1cb43f9739
zhengjun.mao.ry <zhengjun.mao.ry@renesas.com>
2018-08-24 18:01:04 +0800

    update

--- com.mwos.ebochs/src/com/mwos/ebochs/ui/view/GILDTView.java
+++ com.mwos.ebochs/src/com/mwos/ebochs/ui/view/GILDTView.java
@@ -5,10 +5,23 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.widgets.Form;
+import org.eclipse.ui.forms.widgets.FormText;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.custom.TableTree;
+import org.eclipse.jface.viewers.TableTreeViewer;
+import org.eclipse.swt.browser.Browser;
 
 public class GILDTView extends ViewPart {
 
 	public static final String ID = "com.mwos.ebochs.ui.view.GILDTView"; //$NON-NLS-1$
+	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
 
 	public GILDTView() {
 	}
@@ -20,6 +33,14 @@ public class GILDTView extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(new GridLayout(1, false));
+		
+		Browser browser = new Browser(container, SWT.NONE);
+	//	browser.setUrl("file:///C:/Users/mao-zhengjun/Desktop/jquery-easyui-1.5.5.7/demo/datagrid/basic.html");
+		browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		browser.setText("<html>asd</html>");
+		formToolkit.adapt(browser);
+		formToolkit.paintBordersFor(browser);
 
 		createActions();
 		initializeToolBar();
@@ -51,5 +72,4 @@ public class GILDTView extends ViewPart {
 	public void setFocus() {
 		// Set the focus
 	}
-
 }
---
+++ com.mwos.ebochs/src/com/mwos/ebochs/ui/view/test/Register.java
@@ -0,0 +1,57 @@
+package com.mwos.ebochs.ui.view.test;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.FillLayout;
+
+public class Register {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Register window = new Register();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(585, 464);
+		shell.setText("SWT Application");
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		Browser browser = new Browser(shell, SWT.NONE);
+
+	}
+
+}
